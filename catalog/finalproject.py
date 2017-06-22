f2965bb69c119b2e0474e47d25fc661112d38b88
Abhishek Ghosh <abhighosh18@gmail.com>
2017-06-22 00:37:25 -0700

    Chore: remove unused files

--- catalog/finalproject.py
+++ catalog/finalproject.py
@@ -600,4 +600,4 @@ def deleteMenuItem(restaurant_id,menu_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=5000)
